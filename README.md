d48b3b51f8247987485dac45cfc8af282c108951
Abror Ilkhamov <AIlkhamov@spartaglobal.com>
2019-11-01 11:09:01 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# mangodb_new1
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: mangodb_new1
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
